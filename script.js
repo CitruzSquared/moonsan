9f084c2275c4e2992575d901940803587aee00da
Shiru <nckim73@gmail.com>
2025-04-13 18:42:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let footnotebox = document.getElementById("footnotebox");
+function changer(x) {
+	id = x + "-text";
+	var f = document.getElementById(id).innerHTML;
+	footnotebox.innerHTML = f;
+	footnotebox.style.display = "block";
+}
+
+function returner() {
+	footnotebox.style.display = "none";
+}
