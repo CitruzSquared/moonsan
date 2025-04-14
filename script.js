6e7964f133b42cbcc43f2fa4e2058cc5795150cd
Shiru <nckim73@gmail.com>
2025-04-14 23:26:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function changer(x) {
 	var footnote = document.getElementById(id).innerText;
 	var footnoteNumber = footnote.substring(0, 3);
 	var footnoteText = footnote.substring(3, footnote.length);
-	footnotecontainer.innerHTML = "<p style='margin-top: 0px; margin-bottom: 10px; color: #c0c0c0'> <b> FOOTNOTE </b> </p> <span style='color:#94886c'>" + footnoteNumber + "</span>" + footnoteText;
+	footnotecontainer.innerHTML = "<p class='footnotetext'> FOOTNOTE </p> <span style='color:#94886c'>" + footnoteNumber + "</span>" + footnoteText;
 	footnotebox.style.bottom = "0px";
 }
 
